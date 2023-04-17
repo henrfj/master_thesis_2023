d4645bdcc3a511cd00ea4a43ceca0e992179e47b
henrfj <henrfj@stud.ntnu.no>
2023-04-17 16:31:06 +0200

    numerical stability notebook

--- DDPG/environments.py
+++ DDPG/environments.py
@@ -192,7 +192,6 @@ class OpenField_v00(gym.Env):
 		##################
 		self.gfx.update_display()
 		##################
-
 class OpenField_v01(gym.Env):
 	"""Custom Environment that follows gym interface.
 	- In this env. the vehicle will be spawned in an open field.
@@ -377,7 +376,6 @@ class OpenField_v01(gym.Env):
 		##################
 		self.gfx.update_display()
 		##################
-
 class OpenField_v10(gym.Env): # THE MILKMAN
 	"""Custom Environment that follows gym interface.
 	- In this env, the goal pose is *GIVEN* in the state space vector; but it is given in driver-coordinated (CCF)
@@ -572,7 +570,6 @@ class OpenField_v10(gym.Env): # THE MILKMAN
 		##################
 		self.gfx.update_display()
 		##################
-
 class ClosedField_v20(gym.Env): # THE MILKMAN
 	"""Custom Environment that follows gym interface.
 	- This environment continous the V10 CCF trend, but adds SC!
@@ -824,7 +821,6 @@ class ClosedField_v20(gym.Env): # THE MILKMAN
 		##################
 		self.gfx.update_display()
 		##################
-
 class ClosedField_v21(gym.Env): # THE MILKMAN
 	"""Custom Environment that follows gym interface.
 	- This environment continous the V10 CCF trend, but adds SC!
@@ -1098,20 +1094,16 @@ class ClosedField_v21(gym.Env): # THE MILKMAN
 		##################
 		self.gfx.update_display()
 		##################
-
-class MPC_environment_v40(gym.Env): # 
+class ClosedField_v22(gym.Env): # THE MILKMAN
 	"""Custom Environment that follows gym interface.
-	- This environments implement the MPC behaviour we are looking for!
-	- Instead of a single actions -> state -> reward, a **series of actions** will be simulated 
+	- Adds knowledge of previously chosen actions, and rewards smooth driving.
 	"""
 	metadata = {'render.modes': ['human']}
 
 	def __init__(self, sim_dt=0.1, decision_dt=0.5, render=False, v_max=8, v_min=-2,
 	       alpha_max=0.8, tau_steering=0.4, tau_throttle=0.4, horizon=200, edge=150,
-		   episode_s=60, starting_point="", mpc=True):
-		super(MPC_environment_v40, self).__init__()
-		#
-		self.mpc = mpc
+		   episode_s=60):
+		super(ClosedField_v21, self).__init__()
 		#
 		self.sim_dt = sim_dt
 		self.decision_dt = decision_dt
@@ -1122,14 +1114,295 @@ class MPC_environment_v40(gym.Env): #
 		self.tau_throttle = tau_throttle
 		self.horizon = horizon
 		self.edge = edge
+		self.selection = -1
 		self.episode_seconds = episode_s
-		# In case we are not starting from scratch
-		self.starting_point = starting_point
 		# Actions is to give input signal for throttle and steering.
 		self.action_space = spaces.Box(low=np.array([-1, -1]), 
 										high=np.array([1, 1]),
 										dtype=np.float32)
 		# Observations space is the goal_x, goal_y (IN CCF), speed and steering angle
+		self.observation_space = spaces.Box(low=np.array([0, 0, -self.v_min, -alpha_max, -1, -1,
+						    								0, 0, 0, 0, 0, 0,
+															0, 0, 0, 0, 0, 0,
+															0, 0, 0, 0, 0, 0,
+															0, 0, 0, 0, 0, 0,
+															0, 0, 0, 0, 0, 0,
+															0, 0, 0, 0, 0, 0]),
+											high=np.array(
+											[200, 200, 	self.v_max, alpha_max, 1, 1,
+	    												self.horizon, self.horizon, self.horizon, self.horizon, self.horizon, self.horizon,
+														self.horizon, self.horizon, self.horizon, self.horizon, self.horizon, self.horizon,
+														self.horizon, self.horizon, self.horizon, self.horizon, self.horizon, self.horizon,
+														self.horizon, self.horizon, self.horizon, self.horizon, self.horizon, self.horizon,
+														self.horizon, self.horizon, self.horizon, self.horizon, self.horizon, self.horizon,
+														self.horizon, self.horizon, self.horizon, self.horizon, self.horizon, self.horizon]),
+											dtype=np.float32)
+		#
+		self.will_render=render
+		if self.will_render: # For displaying
+			MAP_DIMENSIONS = (1080, 1920)
+			self.gfx = Visualization(MAP_DIMENSIONS, pixels_per_unit=7) # Also initializes the display
+		self.objects = []
+
+	def add_objects(self, vertices):
+		self.objects.append(Object(np.array([0, 0]), vertices=vertices))
+
+	def generate_new_goal_state(self):
+		prev_selection = self.selection
+		selection = -1
+		while selection == prev_selection:
+			selection = np.random.randint(0, 4) # [low, high)
+		if selection == 0: # WEST
+			goal = np.array([25, 75])
+		elif selection == 1: # NORTH
+			# ADD LID - NOTE was too hard, for now... :(
+			#vertices = np.array([[120, 65], [144, 65]])
+			#self.objects.append(Object(np.array([0, 0]), vertices=vertices))
+			goal = np.array([132, 25])
+			#self.vehicle.heading=-np.pi/2
+		elif selection == 2: # EAST
+			goal = np.array([245, 75])
+		elif selection == 3: # SOUTH
+			goal = np.array([132, 135])
+			# ADD LID
+			#vertices = np.array([[120, 85], [144, 85]])
+			#self.objects.append(Object(np.array([0, 0]), vertices=vertices))
+			#self.vehicle.heading=np.pi/2
+		self.goal_x, self.goal_y = goal
+		
+	def reset(self):
+		# Volkswagen
+		self.vehicle = Vehicle(np.array([132, 75]), length=4, width=2,
+								heading=-np.pi/2, tau_steering=self.tau_steering, tau_throttle=self.tau_throttle, dt=self.sim_dt)
+		
+		#######################################
+		# Spawn in the outer wall & obstacles #
+		#######################################
+		vertices = np.array([[5, 5], [5, 150], [270, 150], [270, 5]])
+		self.outer_rim = Object(np.array([0, 0]), vertices=vertices)
+		self.objects = [self.outer_rim]
+		# Add the base walls
+		vertices = np.array([[35, 90], [35, 60], [40, 60], [40, 90]])
+		self.objects.append(Object(np.array([0, 0]), vertices=vertices))
+		vertices = np.array([[115, 35], [115, 40], [145, 40], [145, 35]])
+		self.objects.append(Object(np.array([0, 0]), vertices=vertices))
+		vertices = np.array([[230, 60], [230, 90], [235, 90], [235, 60]])
+		self.objects.append(Object(np.array([0, 0]), vertices=vertices))
+		vertices = np.array([[115, 125], [115, 130], [145, 130], [145, 125]])
+		self.objects.append(Object(np.array([0, 0]), vertices=vertices))
+		# Add side tunnels
+		#vertices = np.array([[120, 65], [120, 85], [115, 85], [115, 65]])
+		#self.objects.append(Object(np.array([0, 0]), vertices=vertices))
+		#vertices = np.array([[144, 65], [144, 85], [149, 85], [149, 65]])
+		#self.objects.append(Object(np.array([0, 0]), vertices=vertices))
+		#Goals and special obstacles added
+		self.generate_new_goal_state()
+		# Last but not least, turn goal states to CCF! (Has to be done after each step as well)
+		self.goal_CCF = self.vehicle.WCFtoCCF(np.array([self.goal_x, self.goal_y]))
+		#######################################
+		#######################################
+		# Determines if the time is up
+		self.time_step = 0
+		#
+		self.goal_threshold = self.vehicle.length*1.2 # Give it some more wiggle room
+		#
+		normed_vel = 0
+		steering_angle = 0
+		#
+		# Get the new static circogram
+		self.SC = self.vehicle.static_circogram_2(N=36, list_objects_simul=self.objects, d_horizon=self.horizon)
+		d1, d2, _, _, _  = self.SC
+		real_distances = d2 - d1
+		#
+		self.previous_throttle = 0
+		self.previous_steering = 0
+		# Generate new state
+		new_state = np.array([self.goal_CCF[0], self.goal_CCF[1], normed_vel, steering_angle, self.previous_throttle, self.previous_steering,
+			real_distances[0], real_distances[1], real_distances[2], real_distances[3], real_distances[4], real_distances[5],
+			real_distances[6], real_distances[7], real_distances[8], real_distances[9], real_distances[10], real_distances[11],
+			real_distances[12], real_distances[13], real_distances[14], real_distances[15], real_distances[16], real_distances[17],
+			real_distances[18], real_distances[19], real_distances[20], real_distances[21], real_distances[22], real_distances[23],
+			real_distances[24], real_distances[25], real_distances[26], real_distances[27], real_distances[28], real_distances[29],
+			real_distances[30], real_distances[31], real_distances[32], real_distances[33], real_distances[34], real_distances[35]
+			],
+			dtype=np.float32)
+
+		return new_state
+
+	def step(self, action):
+		"""Execute one time step within the environment"""
+
+		# Translate action signals to steering signals
+		throttle_signal = action[0]
+		if throttle_signal >= 0:
+			v_ref = self.v_max*throttle_signal
+		else:
+			v_ref = -self.v_min*throttle_signal
+		steering_signal = action[1]
+		alpha_ref = self.alpha_max*steering_signal
+
+		# Call upon the vehicle step action
+		# NOTE Do not need to take new decision every simulation step (0.1 sec)
+		times = np.int32(self.decision_dt/self.sim_dt)
+		if self.will_render:
+			self.render_frames = []
+		for _ in range(times):
+			self.vehicle.one_step_algorithm(alpha_ref, v_ref)
+			# For rendering in sim time
+			xpos, ypos = self.vehicle.position_center
+			heading = self.vehicle.heading
+			# 
+			if self.will_render:
+				self.render_frames.append([xpos, ypos, heading])
+		
+		# After running n simulations steps:
+		xpos, ypos = self.vehicle.position_center
+		heading = self.vehicle.heading
+
+		# Need to multiply by actual direction, as if not - it will always be a positive value.
+		normed_velocity = np.linalg.norm(self.vehicle.X[2:])*self.vehicle.actual_direction
+		steering_angle = self.vehicle.alpha
+
+		# Update goal poses in CCF (as CCF's origin has moved)
+		self.goal_CCF = self.vehicle.WCFtoCCF(np.array([self.goal_x, self.goal_y]))
+
+		# Get the new static circogram
+		self.SC = self.vehicle.static_circogram_2(N=36, list_objects_simul=self.objects, d_horizon=self.horizon)
+		d1, d2, _, _, _  = self.SC
+		self.vehicle.collision_check(d1, d2)
+		real_distances = d2 - d1
+
+		# Generate new state
+		new_state = np.array([self.goal_CCF[0], self.goal_CCF[1], normed_velocity, steering_angle, self.previous_throttle, self.previous_steering, 
+			real_distances[0], real_distances[1], real_distances[2], real_distances[3], real_distances[4], real_distances[5],
+			real_distances[6], real_distances[7], real_distances[8], real_distances[9], real_distances[10], real_distances[11],
+			real_distances[12], real_distances[13], real_distances[14], real_distances[15], real_distances[16], real_distances[17],
+			real_distances[18], real_distances[19], real_distances[20], real_distances[21], real_distances[22], real_distances[23],
+			real_distances[24], real_distances[25], real_distances[26], real_distances[27], real_distances[28], real_distances[29],
+			real_distances[30], real_distances[31], real_distances[32], real_distances[33], real_distances[34], real_distances[35]
+			],
+			dtype=np.float32)
+
+
+		######################################
+		# Reward function! (Sparse for now?) #
+		######################################
+		reward = 0
+		done = False
+		info = "..."
+
+		# Calculate goal distance
+		dist = np.linalg.norm(self.goal_CCF)
+		# Goal is reached!
+		if dist < self.goal_threshold:  # some threshold
+			reward += 40  # Goal reached!
+			done = True
+			info = "'Goal reached!'"
+
+		else:  # punish for further distance ( hill climb? )
+			# NOTE hill climber only gives flat negative reward...
+			reward += -dist*0.01
+
+		if self.vehicle.collided:
+			done = True
+			reward = -10
+			info = "'Collided'"
+
+		# Time is up?
+		elif self.time_step > self.episode_seconds*1/self.decision_dt:  # (30 sek)
+			done = True
+			reward += -10  # Goal not reached :(
+			info = "'Time is up!'"
+		self.time_step += 1
+
+		# Punish jerk: [-2, 2]
+		reward -= np.abs(action[0] - self.previous_throttle)
+		reward -= np.abs(action[1] - self.previous_steering)
+		self.previous_throttle = action[0]
+		self.previous_steering = action[1]
+		return new_state, reward, done, info
+
+	def render(self, mode='human', close=False, render_all_frames=False, show_SC=False):
+		if render_all_frames:
+			for _, frame in enumerate(self.render_frames):
+				self.render_one_frame(frame, show_SC=show_SC)
+		else:
+			self.render_one_frame(np.array([self.vehicle.X[0], self.vehicle.X[1], self.vehicle.heading]), show_SC=show_SC)
+
+	def render_one_frame(self, state, show_SC=False):
+		##################
+		self.gfx.clear_canvas()
+    	##################
+		# Extract geometry
+		length=self.vehicle.length
+		width=self.vehicle.width
+		center_pos = state[0:2]
+		heading = state[2]
+
+		# Vertices:
+		verticesCCF = [np.array([width/2,  length/2 ]),
+						np.array([-width/2, length/2 ]),
+						np.array([-width/2, -length/2]),
+						np.array([width/2,  -length/2])]
+
+		angle = heading-np.pi/2
+		R_W_V = np.array([[np.cos(angle), -np.sin(angle)],
+							[np.sin(angle), np.cos(angle)]])
+
+		verticesWCF = []
+		for vertex in verticesCCF:
+			verticesWCF.append(R_W_V@vertex + np.asarray(center_pos))
+		# Sides
+		sides = [[verticesWCF[-1], verticesWCF[0]]]
+		for i in range(len(verticesWCF)-1):
+			sides.append([verticesWCF[i], verticesWCF[i+1]])
+
+		self.gfx.draw_sides(sides)
+		# Draw heading and center of vehicle
+		self.gfx.draw_center_and_headings_simple(heading, center_pos)
+
+		# Draw the goal and goal limit
+		self.gfx.draw_goal_state(np.array([self.goal_x, self.goal_y]), threshold=self.goal_threshold)
+
+		# Draw all static obstacles
+		for obj in self.objects:
+			self.gfx.draw_sides(obj.sides)
+
+		if show_SC:
+			self.gfx.draw_static_circogram_data(self, self.SC, self.vehicle)
+		##################
+		self.gfx.update_display()
+		##################
+class MPC_environment_v40(gym.Env): # 
+	"""Custom Environment that follows gym interface.
+	- This environments implement the MPC behaviour we are looking for!
+	- Halucinated as well as real gaming
+	"""
+	metadata = {'render.modes': ['human']}
+
+	def __init__(self, sim_dt=0.1, SC_dt=0.5, render=False, v_max=8, v_min=-2,
+	       alpha_max=0.8, tau_steering=0.4, tau_throttle=0.4, horizon=200, edge=150,
+		   episode_s=60, mpc=True):
+		super(MPC_environment_v40, self).__init__()
+		#
+		self.mpc = mpc
+		#
+		self.sim_dt = sim_dt
+		self.SC_dt = SC_dt
+		self.v_max = v_max
+		self.v_min = v_min
+		self.alpha_max = alpha_max
+		self.tau_steering = tau_steering
+		self.tau_throttle = tau_throttle
+		self.horizon = horizon
+		self.edge = edge
+		self.episode_seconds = episode_s
+
+		# Actions is to give input signal for throttle and steering.
+		self.action_space = spaces.Box(low=np.array([-1, -1]), 
+										high=np.array([1, 1]),
+										dtype=np.float32)
+		# Observations space is the goal_x, goal_y (IN CCF), speed and steering angle, as well as previous actions
 		self.observation_space = spaces.Box(low=np.array([0, 0, -self.v_min, -alpha_max, -1, -1,
 						    								0, 0, 0, 0, 0, 0,
 															0, 0, 0, 0, 0, 0,
@@ -1230,18 +1503,10 @@ class MPC_environment_v40(gym.Env): #
 			],
 			dtype=np.float32)
 		
-		#if self.mpc: # Will be used, for planning trajectories - to then be executed
-		#	self.vision_box = Object(np.array([0, 0]), vertices=P2)
-		#	self.trajectory = []
-		#	
-		#	# Two methods for updating vision
-		#	self.update_vision_box_counter = 0
-		#	self.SCs = [] # Would be used to compare and update, 
-
 		return new_state
 
 	def step(self, action):
-		"""Execute one decision time step within the environment"""
+		"""Execute an actual decision step, in the environment"""
 
 		# Translate action signals to steering signals
 		throttle_signal = action[0]
@@ -1252,20 +1517,12 @@ class MPC_environment_v40(gym.Env): #
 		steering_signal = action[1]
 		alpha_ref = self.alpha_max*steering_signal
 
-		# Call upon the vehicle step action
-		# NOTE Do not need to take new decision every simulation step (0.1 sec)
-		times = np.int32(self.decision_dt/self.sim_dt)
-		if self.will_render:
-			self.render_frames = []
-		for _ in range(times):
-			self.vehicle.one_step_algorithm(alpha_ref, v_ref)
-			# For rendering in sim time
-			xpos, ypos = self.vehicle.position_center
-			heading = self.vehicle.heading
-			# 
-			if self.will_render:
-				self.render_frames.append([xpos, ypos, heading])
-		
+		# Call upon the vehicle step action)
+		self.vehicle.one_step_algorithm_2(alpha_ref, v_ref, dt=self.sim_dt)
+		# For rendering in sim time
+		xpos, ypos = self.vehicle.position_center
+		heading = self.vehicle.heading
+
 		# After running n simulations steps:
 		xpos, ypos = self.vehicle.position_center
 		heading = self.vehicle.heading
--- DDPG/main_torch.py
+++ DDPG/main_torch.py
@@ -1,9 +1,9 @@
-from ddpg_torch import Agent
+from ddpg_torch import Agent, MPC_Agent
 import gym
 import numpy as np
 import os
 # My own env!
-from environments import OpenField_v00, OpenField_v01, OpenField_v10, ClosedField_v20, ClosedField_v21
+from environments import OpenField_v00, OpenField_v01, OpenField_v10, ClosedField_v20, ClosedField_v21, MPC_environment_v40
 from utils import plotLearning
 
 def open_field_v00_training(episodes=5000, sim_dt=0.1, decision_dt=0.1):
@@ -214,17 +214,89 @@ def v21_training(episodes=5000, sim_dt=0.1,decision_dt=0.1, chkpt_dir="DDPG/chec
 
     plotLearning(score_history, filename, window=100)
 
-def v40_MPC_training(episodes=5000, sim_dt=0.1,decision_dt=0.1, plotting = 'DDPG/plots/openfield_v21.png', save_folder="DDPG/checkpoints/v21", loadfolder=None):
-
+def v22_training(episodes=5000, sim_dt=0.1,decision_dt=0.1, chkpt_dir="DDPG/checkpoints/v22", filename = 'DDPG/plots/openfield_v22.png'):
+    """
+    Here, we added knowledge of previous action, and punish for jerk.
+    """
     env = ClosedField_v21(sim_dt=sim_dt, decision_dt=decision_dt, render=False, v_max=20, v_min=-4, alpha_max=0.5, tau_steering=0.5, tau_throttle=0.5, horizon=200)
+    best_score = -10000 # Impossibly bad
 
-    agent = Agent(alpha=0.000025, beta=0.00025, input_dims=[40], tau=0.1, env=env, tau=0.001
-                batch_size=64,  layer1_size=400, layer2_size=300, n_actions=2, chkpt_dir=save_folder)
+    agent = Agent(alpha=0.000025, beta=0.00025, input_dims=[40], tau=0.1, env=env, #alpha=0.000025, beta=0.00025, tau=0.001
+                batch_size=64,  layer1_size=400, layer2_size=300, n_actions=2, chkpt_dir=chkpt_dir)
+    np.random.seed(42)
+
+    score_history = []
+    for i in range(episodes):
+        obs = env.reset()
+        done = False
+        score = 0
+        episode_lenght = 0
+        while not done:
+            act = agent.choose_action(obs)
+            new_state, reward, done, info = env.step(act)
+            agent.remember(obs, act, reward, new_state, int(done))
+            agent.learn()
+            score += reward
+            obs = new_state
+            #env.render()
+            episode_lenght += 1
+
+        score_history.append(score)
+        
+        # Store best average models
+        avg_score = np.mean(score_history[-100:])
+        if avg_score > best_score:
+            best_score = avg_score
+            agent.save_models()
+
+        print('episode ', i, 'score %.2f' % score,
+            'trailing 100 games avg %.3f' % np.mean(score_history[-100:]), "ep_lenght:", episode_lenght, "info:", info)
+
+    plotLearning(score_history, filename, window=100)
+
+
+def v40_MPC_training(episodes=5000, sim_dt=0.1, SC_dt=0.5, plotting = 'DDPG/plots/mpc_v40.png', save_folder="DDPG/checkpoints/v40", loadfolder="DDPG/checkpoints/v21_2"):
+    env = MPC_environment_v40(sim_dt=0.1, SC_dt=0.5, render=False, v_max=8, v_min=-2,
+	       alpha_max=0.8, tau_steering=0.4, tau_throttle=0.4, horizon=200, edge=150,
+		   episode_s=60, mpc=True)
+    agent = MPC_Agent(alpha=0.000025, beta=0.00025, input_dims=[40], tau=0.1, env=env, 
+            batch_size=64,  layer1_size=400, layer2_size=300, n_actions=2, chkpt_dir=save_folder)
+    
     if loadfolder:
         # TODO: specify where to load models from!
-        agent.load_models()
+        agent.load_models(load_directory=loadfolder)
     
     np.random.seed(0)
+    #####################################################
+    score_history = []
+    for i in range(episodes):
+        obs = env.reset()
+        done = False
+        score = 0
+        episode_lenght = 0
+        while not done:
+            act = agent.choose_action(obs)
+            new_state, reward, done, info = env.step(act)
+            agent.remember(obs, act, reward, new_state, int(done))
+            agent.learn()
+            score += reward
+            obs = new_state
+            #env.render()
+            episode_lenght += 1
+
+        score_history.append(score)
+        
+        # Store best average models
+        avg_score = np.mean(score_history[-100:])
+        if avg_score > best_score:
+            best_score = avg_score
+            agent.save_models()
+
+        print('episode ', i, 'score %.2f' % score,
+            'trailing 100 games avg %.3f' % np.mean(score_history[-100:]), "ep_lenght:", episode_lenght, "info:", info)
+
+    plotLearning(score_history, plotting, window=100)
+
 
 if __name__ =="__main__":
     # IN WCF
@@ -234,4 +306,6 @@ if __name__ =="__main__":
     #open_field_v10_training(episodes=5000, sim_dt=0.1, decision_dt=0.1, chkpt_dir="DDPG/checkpoints/v10_1", filename = 'DDPG/plots/openfield_v10_1.png')
     # Obstacles
     #v20_training(episodes=10000, sim_dt=0.1, decision_dt=0.1, chkpt_dir="DDPG/checkpoints/v20_2", filename = 'DDPG/plots/openfield_v20_2.png')
-    v21_training(episodes=50000, sim_dt=0.1, decision_dt=0.5, chkpt_dir="DDPG/checkpoints/v21_2", filename = 'DDPG/plots/openfield_v21_2.png')
+    #v21_training(episodes=50000, sim_dt=0.1, decision_dt=0.5, chkpt_dir="DDPG/checkpoints/v21_2", filename = 'DDPG/plots/openfield_v21_2.png')
+    v22_training(episodes=50000, sim_dt=0.1, decision_dt=0.1, chkpt_dir="DDPG/checkpoints/v22", filename = 'DDPG/plots/openfield_v22.png') # Added jerk control
+    # MPC
